32cae545543c5611d6cff1c9b25814dd96983672
Eduardo San Martin Morote <posva13@gmail.com>
2017-07-21 11:15:24 +0200

    handle user registration with bcrypt

--- starter-code/controllers/auth.js
+++ starter-code/controllers/auth.js
@@ -1,8 +1,26 @@
 const { Router } = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const router = Router()
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup')
+})
+
+router.post('/signup', (req, res) => {
+  const { email, password } = req.body
+  const salt = bcrypt.genSaltSync(10)
+  const user = new User({
+    email,
+    password: bcrypt.hashSync(password, salt)
+  })
+
+  user.save()
+  .then((user) => {
+    res.redirect('/login')
+  }).catch((err) => {
+    res.redirect('/signup')
+  })
 })
 
 module.exports = router
